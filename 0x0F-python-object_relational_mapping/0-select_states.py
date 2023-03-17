80933222ebc0985c1104388a6171a18937b96988
IreneInHerSpace <insiteketa@gmail.com>
2023-03-17 19:02:50 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""Script that lists all states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py username password database")
+        exit()
 
-def get_states():
-    """Takes arguments argv to list from database
-
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3],
+                         port=3306)
 
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     rows = cur.fetchall()
-    for i in rows:
-        print(i)
+
+    for row in rows:
+        print(row)
 
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    get_states()
