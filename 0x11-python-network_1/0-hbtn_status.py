77e270f1208ae59eb6d492532ada89bb3655f32d
IreneInHerSpace <insiteketa@gmail.com>
2023-04-01 14:36:42 +0200

    fix error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-
-def fetcher():
-    """fetcher"""
+url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
