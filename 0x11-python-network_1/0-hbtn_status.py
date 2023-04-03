7f663c58006abd83a2652ad32b8e5d4cd6f99c34
IreneInHerSpace <insiteketa@gmail.com>
2023-04-03 22:07:43 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,13 +6,8 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
         html = response.read()
-<<<<<<< HEAD
 
-
-=======
         
-        
->>>>>>> d00a07ebf80c8e1384480bef7428bc72e88420a6
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
