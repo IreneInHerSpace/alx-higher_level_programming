aa27e0192622f91f041d0e9439cb1dc9620318c4
irene <insiteketa@gmail.com>
2023-04-03 21:22:27 +0000

    add task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
+"""
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and displays the response body.
+"""
+
 import sys
-
-
-def post():
-    """status"""
-    result = requests.post(sys.argv[1], data={"email": sys.argv[2]})
-
-    print(result.text)
+import requests
 
 if __name__ == "__main__":
-    post()
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    print(response.text)
