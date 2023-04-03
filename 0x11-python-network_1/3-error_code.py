079be16e575c575ae4b479f0f04dd957366a30e3
irene <insiteketa@gmail.com>
2023-04-03 20:26:08 +0000

    update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""Send request"""
 import urllib.request
 import urllib.error
 import sys
 
+url = sys.argv[1]
 
-def sender():
-    """sender"""
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
-if __name__ == "__main__":
-    sender()
+try:
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print('Error code: {}'.format(e.code))
