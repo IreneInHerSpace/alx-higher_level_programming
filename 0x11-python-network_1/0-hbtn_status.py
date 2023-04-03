bff94f88642b6a048b7093a5d28f28ea78024e5f
irene <insiteketa@gmail.com>
2023-04-03 20:04:56 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status/') as response:
         html = response.read()
+
+
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
