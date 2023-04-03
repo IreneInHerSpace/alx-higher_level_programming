306c0af0e55f9b9342e6fea84c63efc4c0c929eb
irene <insiteketa@gmail.com>
2023-04-03 20:20:57 +0000

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Check status"""
 import requests
 
+url = 'https://alx-intranet.hbtn.io/status'
+response = requests.get(url)
 
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
-
-if __name__ == "__main__":
-    status()
+print('Body response:')
+print('\t- type: {}'.format(type(response.text)))
+print('\t- content: {}'.format(response.text))
