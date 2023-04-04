62a90e26aac91a64dddf927927a4b5c88ee16835
irene <insiteketa@gmail.com>
2023-04-04 11:19:32 +0000

    add file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers: A list of integers.
+
+    Returns:
+        The peak element in the list.
+
+    Raises:
+        ValueError: If the list is empty.
+    """
+
+    # Check for empty list
     if not list_of_integers:
-        return None
+        raise ValueError('List is empty')
 
-    low = 0
-    high = len(list_of_integers) - 1
+    # Define start and end indices
+    start, end = 0, len(list_of_integers) - 1
 
-    while low < high:
-        mid = (low + high) // 2
+    # Binary search to find the peak
+    while start < end:
+        mid = (start + end) // 2
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
+            end = mid
         else:
-            low = mid + 1
+            start = mid + 1
 
-    return list_of_integers[low]
+    return list_of_integers[start]
